929f1178c1a43efb155ca087dacf7cffe7020db0
Yuan <2365603880@qq.com>
2020-08-21 02:23:24 +0800

    ADD#添加OprtNotCHange

---
+++ JavaStudy/src/Day02/OprtNotChange.java
@@ -0,0 +1,12 @@
+package Day02;
+
+public class OprtNotChange {
+    public static void main(String[] args){
+        int a = 100;
+        System.out.println(a + 1);
+        System.out.println(a);
+
+        a = a + 10;
+        System.out.println(a);
+    }
+}
