993dcc4720c96e0918494d536f190063a057cc0b
Yuan <2365603880@qq.com>
2020-08-20 19:35:36 +0800

    ADD#添加JavaStudy,ADD#添加Day01

---
+++ JavaStudy/src/Day01/HelloWorld.java
@@ -0,0 +1,9 @@
+package Day01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World! Go Go Go!");
+        System.out.println("你好  世界!");
+    }
+}
