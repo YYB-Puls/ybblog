20b56480c59161b3095c63b4754e98ea79af3ff8
Yuan <2365603880@qq.com>
2020-08-21 02:34:05 +0800

    ADD#添加IntgerDiv

---
+++ JavaStudy/src/Day02/IntegerDiv.java
@@ -0,0 +1,26 @@
+package Day02;
+
+public class IntegerDiv {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println(a / b);
+
+        int c = 10;
+        int d = 15;
+        int x = 10;
+        System.out.println(a >b);
+        System.out.println(a <b);
+        System.out.println(a !=b);
+        System.out.println(a <=b);
+        System.out.println(a ==b);
+        System.out.println(a >=b);
+
+        System.out.println(a > c);
+        System.out.println(a < c);
+        System.out.println(a >= c);
+        System.out.println(a <= c);
+        System.out.println(a != c);
+        System.out.println(a == c);
+    }
+}
