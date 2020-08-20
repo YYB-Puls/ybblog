8e2ed7a8b6bd58f0e19cf3f83d41dfed397bdb81
Yuan <2365603880@qq.com>
2020-08-21 02:50:01 +0800

    ADD#添加ListralNumber

---
+++ JavaStudy/src/Day02/ListralNumber.java
@@ -0,0 +1,15 @@
+package Day02;
+
+public class ListralNumber {
+    public static void main(String[] args) {
+        int a = 05;
+        int b = 011;
+        int c = 0xf;
+        int d = 0x11;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
