e404b3f8848881252b86e33f3446e93f9a80db84
Yuan <2365603880@qq.com>
2020-08-21 01:59:12 +0800

    ADD#添加BigNumber

---
+++ JavaStudy/src/Day02/BigNumber.java
@@ -0,0 +1,10 @@
+package Day02;
+
+public class BigNumber {
+    public static void main(String[] args){
+        int bigNum = 999999999;
+        long number = 999999999999l;
+
+        System.out.println(number);
+    }
+}
