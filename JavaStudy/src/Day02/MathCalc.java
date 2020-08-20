7d8692f20725ba76496517d8741dc837fcaf67ad
Yuan <2365603880@qq.com>
2020-08-21 01:43:49 +0800

    ADD#添加Day02

---
+++ JavaStudy/src/Day02/MathCalc.java
@@ -0,0 +1,9 @@
+package Day02;
+
+public class MathCalc {
+    public static void main(String[] args){
+        System.out.println(5+6);
+        System.out.println(3+6*9-8/3.0);
+        System.out.println(2*3+3*6*6+4*6*6*6);
+    }
+}
---
+++ JavaStudy/src/Day02/Variable.java
@@ -0,0 +1,14 @@
+package Day02;
+
+public class Variable {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+        int c = 5;
+
+        int x = 10;
+        int y = a*b*b+c*b*b*b+x*b*b*b*b;
+
+        System.out.println(y);
+    }
+}
