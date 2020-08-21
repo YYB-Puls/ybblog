8da36c6a7e87ec8aa0ad155471a13f5d084ff8f1
Yuan <2365603880@qq.com>
2020-08-21 13:54:32 +0800

    ADD#添加InceraseDecrease

---
+++ JavaStudy/src/Day02/IncreaseDecrease.java
@@ -0,0 +1,13 @@
+package Day02;
+
+public class IncreaseDecrease {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("a++=" + a++);
+        System.out.println(a);
+
+        int b = 9;
+        System.out.println("b--=" + b--);
+        System.out.println(b);
+    }
+}
