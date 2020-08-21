dd90ede9740191fc8ed36d9e2b396a569a519fa7
Yuan <2365603880@qq.com>
2020-08-21 16:12:28 +0800

    Update#更新CalcDivFor

--- JavaStudy/src/Day03/CalcDivFor.java
+++ JavaStudy/src/Day03/CalcDivFor.java
@@ -2,12 +2,20 @@ package Day03;
 
 public class CalcDivFor {
     public static void main(String[] args) {
-        int a = 100;
+        int a = 110;
         int b = 3;
+
+        int found = 0;
+        int toBeFund = 10;
 
         for (int i = 0; i < 100; i++){
             if (a % b == 0){
-                System.out.println(a+"除"+b+"可以整除\t"+(a/b));
+                System.out.println(a+"除"+b+"可以整除\t商为:"+(a/b));
+                found ++;
+            }
+            if (found >= toBeFund){
+                System.out.println("已经找到"+toBeFund+"个数,循环退出");
+                break;
             }
             a++;
         }
