629863e458ca841ddd1da08882fc91124fc9b6b2
Yuan <2365603880@qq.com>
2020-08-21 14:11:09 +0800

    ADD#添加IfElseBaoZi

---
+++ JavaStudy/src/Day03/IfElseBaoZi.java
@@ -0,0 +1,15 @@
+package Day03;
+
+public class IfElseBaoZi {
+    public static void main(String[] args) {
+        int BaoZi = 3;
+        boolean BaoziGangcchulong = true;
+
+        if (BaoziGangcchulong){
+            BaoZi += 2;
+            System.out.println("包子刚出笼多买了两个包子,一共买了"+BaoZi+"个包子");
+        }else {
+            System.out.println("包子不是刚出笼,只买了"+BaoZi+"个包子");
+        }
+    }
+}
