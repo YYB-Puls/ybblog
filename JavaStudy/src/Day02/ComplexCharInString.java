35d101cdb5688f853390c064fc70a360aa9582e6
Yuan <2365603880@qq.com>
2020-08-21 13:48:30 +0800

    ADD#添加ComplexCharInString

---
+++ JavaStudy/src/Day02/ComplexCharInString.java
@@ -0,0 +1,8 @@
+package Day02;
+
+public class ComplexCharInString {
+    public static void main(String[] args) {
+        String content = "a\tb\t\"c\td\nx";
+        System.out.println(content);
+    }
+}
