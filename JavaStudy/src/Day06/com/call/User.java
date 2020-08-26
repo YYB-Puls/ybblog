a2f5f12d15e7bb5acb7d94bd1e59e93923c0d14a
Yuan <2365603880@qq.com>
2020-08-26 09:58:16 +0800

    Update#用户家里没有葡萄,吃苹果

--- JavaStudy/src/Day06/com/call/User.java
+++ JavaStudy/src/Day06/com/call/User.java
@@ -3,13 +3,13 @@ package Day06.com.call;
 public class User {
     public String name;
     public int password;
-    
+
 
     public void Login(){
         System.out.println("用户"+name+",密码"+password);
     }
 
     public void eat(){
-        System.out.println("用户吃葡萄!");
+        System.out.println("用户吃苹果!");
     }
 }
