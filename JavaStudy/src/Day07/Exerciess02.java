1201cd83eaf7505f5d9c04c281d4d89057331605
Yuan <2365603880@qq.com>
2020-08-28 11:15:15 +0800

    ADD#4.10习题2,使用嵌套for语句循环输出

---
+++ JavaStudy/src/Day07/Exerciess02.java
@@ -0,0 +1,13 @@
+package Day07;
+
+public class Exerciess02 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++){
+
+            for (int j = 1; j <= 9; j++){
+                System.out.print(j*i+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
