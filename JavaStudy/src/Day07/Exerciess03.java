7a5ca03367f181ad8a5b1b5d93892b9e5b249aa0
Yuan <2365603880@qq.com>
2020-08-28 11:45:55 +0800

    ADD#4.10习题3,while语句循环输出

---
+++ JavaStudy/src/Day07/Exerciess03.java
@@ -0,0 +1,12 @@
+package Day07;
+
+public class Exerciess03 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 9){
+            System.out.print(i+"\t");
+            System.out.println(2*i+"\t"+3*i+"\t"+4*i+"\t"+5*i+"\t"+6*i+"\t"+7*i+"\t"+8*i+"\t"+9*i);
+            i ++;
+        }
+    }
+}
