c01f4d119b68001eae2d38df192bfbf271516850
Yuan <2365603880@qq.com>
2020-08-29 06:43:40 +0800

    ADD#数组的使用

---
+++ JavaStudy/src/Day07/ArrayElement.java
@@ -0,0 +1,11 @@
+package Day07;
+
+public class ArrayElement {
+    public static void  main(String[] args){
+        int[] intArray =new int[1];
+        byte byteValue = 55;
+        intArray[0] = byteValue;
+        System.out.println(intArray[0]);
+    }
+
+}
