217bf180c1dfabd9aa3923bdd484ed01cf4c8e79
Yuan <2365603880@qq.com>
2020-09-01 14:16:26 +0800

    ADD#数组习题1:定义一个21个元素的数组,给数组元素的值为其数组下标的值

---
+++ JavaStudy/src/Day08/Exerciess01.java
@@ -0,0 +1,11 @@
+package Day08;
+
+public class Exerciess01 {
+    public static void main(String[] args) {
+        int[] ShuZu = new int[21];
+        for (int i = 0; i < ShuZu.length; i++){
+            ShuZu[i] = i;
+            System.out.println(ShuZu[i]);
+        }
+    }
+}
