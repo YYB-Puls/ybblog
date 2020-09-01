6c7bad93a3cdbe6feccda59c2da09adea67c99aa
Yuan <2365603880@qq.com>
2020-09-01 14:31:18 +0800

    ADD#习题3:根据习题2的案例,给第二维的下标为0的元素赋值为1,下标为2的赋值为2,控制台输出

--- JavaStudy/src/Day08/Exerciess03.java
+++ JavaStudy/src/Day08/Exerciess03.java
@@ -1,13 +1,18 @@
 package Day08;
 
-public class Exerciess02 {
+public class Exerciess03 {
     public static void main(String[] args) {
         int a = 10;
         int b = 2;
         int[][] ShuZu = new int[a][b];
         for (int i = 0; i < ShuZu.length; i++){
             for (int j = 0; j < b; j++){
-                ShuZu[i][j] = 1;
+                if (ShuZu[i][j] == ShuZu[i][0] ){
+                    ShuZu[i][j] = 1;
+                }
+                if (ShuZu[i][j] == ShuZu[i][1]){
+                    ShuZu[i][j] = 2;
+                }
                 System.out.print(ShuZu[i][j]+"\t");
             }
             System.out.println();
