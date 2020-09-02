c8580212dc0f6a7fef260ba46ffa21ebd6ff533f
Yuan <2365603880@qq.com>
2020-09-02 16:41:14 +0800

    ADD#构造方法的使用

---
+++ JavaStudy/src/Day09/Car.java
@@ -0,0 +1,17 @@
+package Day09;
+
+public class Car {
+    int speed;
+    String color;
+    String name;
+    String direaction;
+
+    public Car(){
+
+    }
+    public Car(String p_name,String p_color){
+        this.color = p_color;
+        this.name = p_name;
+
+    }
+}
---
+++ JavaStudy/src/Day09/TestCar.java
@@ -0,0 +1,8 @@
+package Day09;
+
+public class TestCar {
+    public static void main(String[] args) {
+        Car car = new Car("球状闪电号","白色");
+        System.out.println("汽车名字为:"+car.name+",颜色是"+car.color);
+    }
+}
