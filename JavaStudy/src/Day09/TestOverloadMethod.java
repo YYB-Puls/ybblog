a367ad83aaf4540937b857ea8d9a85dcdd0d23da
Yuan <2365603880@qq.com>
2020-09-02 16:16:13 +0800

    ADD#初识重载

---
+++ JavaStudy/src/Day09/TestOverloadMethod.java
@@ -0,0 +1,12 @@
+package Day09;
+
+public class TestOverloadMethod {
+    public static void main(String[] args) {
+        CarOverload carol = new CarOverload();
+        carol.raiseSpeed(9999);
+        System.out.println("调用签名为raiseSpeed(int)的加速方法,现在车速为:"+carol.speed);
+        carol.speed = 0;
+        carol.raiseSpeed(999999, 88);
+        System.out.println("现在的车速为:"+carol.speed);
+    }
+}
