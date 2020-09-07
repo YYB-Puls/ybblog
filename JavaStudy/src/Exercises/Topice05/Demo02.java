e048bc49d9f496a518c5684dd2859446be0d5936
zhonglunshun <zhonglunshun@skyruler.cn>
2020-09-07 09:40:30 +0800

    Fix#完成前六章的作业批改

--- JavaStudy/src/Exercises/Topice05/Demo02.java
+++ JavaStudy/src/Exercises/Topice05/Demo02.java
@@ -11,7 +11,8 @@ public class Demo02 {
         int a = 10;
         int b = 2;
         int[][] ShuZu = new int[a][b];
-        // todo 这里是让你给这个二维数组所有元素赋值为1并获取数组长度（而不是写死的数字）将数组打印出来
+        // todo 请将习题二补全或者使用差量提交的方式
+        // todo 使用两个引用而不是一个引用，注意审题
         for (int i = 0; i < ShuZu.length; i++){
             for (int j = 0; j < b; j++){
                 if (ShuZu[i][j] == ShuZu[i][0] ){
--- JavaStudy/src/Exercises/Topice06/TestBook.java
+++ JavaStudy/src/Exercises/Topice06/TestBook.java
@@ -3,6 +3,8 @@ package Exercises.Topice06;
 /**
  * 使用第二题中添加的方法创建book类的对象,然后给Book类的属性赋值,
  * 最后将属性值输出到控制台
+ *
+ * todo 习题三去哪里了？
  */
 
 public class TestBook {
