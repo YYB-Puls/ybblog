61c596a42fb72b68f0d10c58bba35a4b6af54886
Yuan <2365603880@qq.com>
2020-09-08 00:55:31 +0800

    Update#更新TODO,第四章习题

---
+++ JavaStudy/src/Day10/Car.java
@@ -0,0 +1,28 @@
+package Day10;
+
+public class Car {
+    //汽车速度
+    public int speed;
+    //汽车名字
+    public String name;
+    //汽车颜色
+    public String color;
+    //汽车提速的方法,参数是汽车需要添加的速度
+    public void speedUp(int p_speed){
+        if (p_speed > 0){
+            speed += p_speed;
+        }
+    }
+    //汽车减速的方法,参数是汽车需要减少的速度
+    public void slowDown(int p_speed){
+        //如果p_speed大于0,则计算新的速度
+        if (p_speed > 0){
+            int tempSpeed =speed - p_speed;
+            //如果新的速度大于0,则给汽车减少相应的速度
+            if (tempSpeed >= 0){
+                speed = tempSpeed;
+            }
+        }
+
+    }
+}
--- JavaStudy/src/Exercises/Topice04/Demo03.java
+++ JavaStudy/src/Exercises/Topice04/Demo03.java
@@ -6,11 +6,18 @@ package Exercises.Topice04;
 public class Demo03 {
     public static void main(String[] args) {
         int i = 1;
+
+
         while (i <= 9){
             System.out.print(i+"\t");
             // todo 表达式太长，请使用while精简
-            System.out.println(2*i+"\t"+3*i+"\t"+4*i+"\t"+5*i+"\t"+6*i+"\t"+7*i+"\t"+8*i+"\t"+9*i);
+            for (int j = 2; j <=9; j++){
+                System.out.print(j*i+"\t");
+            }
+            System.out.println();
+            /*System.out.println(2*i+"\t"+3*i+"\t"+4*i+"\t"+5*i+"\t"+6*i+"\t"+7*i+"\t"+8*i+"\t"+9*i);*/
             i ++;
+
         }
     }
 }
