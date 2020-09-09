7a21d6cdc3297c1ff2d5b055b1953f64e14d93c3
Yuan <2365603880@qq.com>
2020-09-09 22:57:28 +0800

    Update#第五章习题3

--- JavaStudy/src/Exercises/Topice05/Demo03.java
+++ JavaStudy/src/Exercises/Topice05/Demo03.java
@@ -9,15 +9,17 @@ public class Demo03 {
     public static void main(String[] args) {
         int a = 10;
         int b = 2;
+        int c = 1;
+        int d = 2;
         int[][] ShuZu = new int[a][b];
         // todo 使用两个引用而不是一个引用，注意审题
         for (int i = 0; i < ShuZu.length; i++){
             for (int j = 0; j < b; j++){
                 if (ShuZu[i][j] == ShuZu[i][0] ){
-                    ShuZu[i][j] = 1;
+                    ShuZu[i][j] = c;
                 }
                 if (ShuZu[i][j] == ShuZu[i][1]){
-                    ShuZu[i][j] = 2;
+                    ShuZu[i][j] = d;
                 }
                 System.out.print(ShuZu[i][j]+"\t");
             }
