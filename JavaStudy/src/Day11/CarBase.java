c1d41eee874b76e52c1578cae849c495169701cd
Yuan <2365603880@qq.com>
2020-09-11 12:49:55 +0800

    ADD#CarBase给变量添加setget

--- JavaStudy/src/Day11/CarBase.java
+++ JavaStudy/src/Day11/CarBase.java
@@ -4,6 +4,22 @@ public class CarBase {
     public int speed;
     public static int MAX_SPEED = 90;
 
+    public int getSpeed (){
+        return speed;
+    }
+
+    public void setSpeed(int p_speed){
+        this.speed = p_speed;
+    }
+
+    public static int getMaxSpeed(){
+        return MAX_SPEED;
+    }
+
+    public static void setMaxSpeed(int MAX_SPEED){
+        MAX_SPEED = MAX_SPEED;
+    }
+
     public void speedUP(int p_speed){
         int tempSpeed = 0;
         if(p_speed > 0){
