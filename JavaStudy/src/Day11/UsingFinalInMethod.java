44a8e365a61ba3ad3838f4c2a536dcbfa13ec48e
Yuan <2365603880@qq.com>
2020-09-11 11:58:58 +0800

    ADD#final修饰的方法不能被覆盖

---
+++ JavaStudy/src/Day11/UsingFinalInMethod.java
@@ -0,0 +1,10 @@
+package Day11;
+
+public class UsingFinalInMethod {
+    public final void finalMethod() {
+        System.out.println("这是一个使用final修饰的方法");
+    }
+    public void commonMethod(){
+        System.out.println("这是一个没有使用final修饰的方法");
+    }
+}
