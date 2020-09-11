99c09a149be1f5b3d0d99bcfb426ac13b5cccf29
Yuan <2365603880@qq.com>
2020-09-11 10:21:46 +0800

    ADD#类的组成名字

---
+++ JavaStudy/src/Day11/ClassA.java
@@ -0,0 +1,10 @@
+package Day11;
+
+public class ClassA {
+    public int instanceVariable;
+    public static int staticVariable;
+    public void method(){
+
+    }
+    public static void staticMethod(){}
+}
