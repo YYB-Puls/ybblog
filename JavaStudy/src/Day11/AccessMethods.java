f44d6cf5ff836f2754162bc583f85ae888095cc1
Yuan <2365603880@qq.com>
2020-09-11 11:21:07 +0800

    ADD#protected访问权限,仅在子类和同级包中访问

--- JavaStudy/src/Day11/AccessMethods.java
+++ JavaStudy/src/Day11/AccessMethods.java
@@ -2,11 +2,11 @@ package Day11;
 
 public class AccessMethods {
     //public修饰的方法
-    public void publicMethod(){}
+    protected void publicMethod(){}
 
 
     //类中的其他方法
-    public void usingMethod(){
+    protected void usingMethod(){
         //this关键字调用方法
         this.publicMethod();
     }
