a976238cf36de1cd4545915293961a437d2eafd5
Yuan <2365603880@qq.com>
2020-09-11 11:23:34 +0800

    ADD#private仅在本类访问

--- JavaStudy/src/Day11/AccessMethods.java
+++ JavaStudy/src/Day11/AccessMethods.java
@@ -2,11 +2,11 @@ package Day11;
 
 public class AccessMethods {
     //public修饰的方法
-     void publicMethod(){}
+    private void publicMethod(){}
 
 
     //类中的其他方法
-     void usingMethod(){
+     private void usingMethod(){
         //this关键字调用方法
         this.publicMethod();
     }
