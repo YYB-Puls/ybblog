72baa5995d001cbb55136c3612e96beb846f7792
Yuan <2365603880@qq.com>
2020-09-11 12:17:39 +0800

    ADD#pow()方法计算次方和ranom()方法生成随机数的使用

---
+++ JavaStudy/src/Day11/UsingMath.java
@@ -0,0 +1,13 @@
+package Day11;
+
+public class UsingMath {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        double result = Math.pow(a,b );
+        System.out.println("a的b次方结果如下:"+result);
+
+        double random = Math.random();
+        System.out.println("本次随机数是"+random);
+    }
+}
