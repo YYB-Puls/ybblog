5e589561a8d710c1bbe3b7a4368711c3d0ba5475
Yuan <2365603880@qq.com>
2020-09-11 19:59:36 +0800

    ADD#强制类型转换出错

--- JavaStudy/src/Day10/AsSuperClass.java
+++ JavaStudy/src/Day10/AsSuperClass.java
@@ -4,10 +4,9 @@ import Day11.CarBase;
 
 public class AsSuperClass {
     public static void main(String[] args) {
-        SportsCar sports = new SportsCar();
-        Car car = sports;
-        car.addN(11);
-        sports.addN(30);
+        Car car = new Car();
+        SportsCar sportsCar = (SportsCar) car;
+        sportsCar.addN(30);
     }
 
 
