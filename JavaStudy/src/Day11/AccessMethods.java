3b4a7d64e7d3d936d09949d07c887a610831d074
Yuan <2365603880@qq.com>
2020-09-11 11:22:25 +0800

    ADD#无修饰符仅在同包中访问

--- JavaStudy/src/Day11/AccessMethods.java
+++ JavaStudy/src/Day11/AccessMethods.java
@@ -2,11 +2,11 @@ package Day11;
 
 public class AccessMethods {
     //public修饰的方法
-    protected void publicMethod(){}
+     void publicMethod(){}
 
 
     //类中的其他方法
-    protected void usingMethod(){
+     void usingMethod(){
         //this关键字调用方法
         this.publicMethod();
     }
