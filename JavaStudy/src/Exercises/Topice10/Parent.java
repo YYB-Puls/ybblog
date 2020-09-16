facc836d47e007703d9e7fa9826f699cf1663b80
zhonglunshun <zhonglunshun@skyruler.cn>
2020-09-16 09:55:38 +0800

    Update#批改作业第十章

--- JavaStudy/src/Exercises/Topice10/Parent.java
+++ JavaStudy/src/Exercises/Topice10/Parent.java
@@ -22,35 +22,6 @@ public class Parent {
         System.out.println("Parent类的method2()方法被调用了");
     }
 
-    /**
-     * 2.联系覆盖的方法:编写一个静态方法invokMethods,参数为
-     * parent类的引用,方法中首先使用instanceof判断参数指向对象所
-     * 属的类,然后调用method1()方法和method2()方法
-     * 然后编写一个程序,分别使用Parent类的实例和Child实例为参数
-     * 调用invokeMethods()方法
-     */
-    //编写一个静态方法invokMethods参数为parent类的引用
-    public static void invokeMethods(Parent parent) {
-        //方法中首先使用instanceof判断参数指向对象所属的类,然后调用method1()方法和method2()方法
-        if (parent instanceof Parent) {
-            parent.method1();
-            parent.method2();
-        }
-        //todo instance用来准确区分不同类型对象的，如果都用父类所有对象继承Object，instanceof就没有意义了
-        if (parent instanceof Child) {
-            Child child = (Child) parent;
-            child.method1();
-            child.method2();
-        }
-
-        if (parent instanceof Child2) {
-            Child2 child = (Child2) parent;
-            child.method1();
-            child.method2();
-        }
-
-    }
-
 
    /* public static void main(String[] args) {
         Parent parent = new Parent();
