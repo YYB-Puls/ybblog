b5377c9ea9787d66fd0f3160d9cdefdba0a1f474
Yuan <2365603880@qq.com>
2020-10-13 12:43:54 +0800

    Updaate#修改18章习题2代码

--- JavaStudy/src/Exercises/Topice18/Exercise2/DeleFile.java
+++ JavaStudy/src/Exercises/Topice18/Exercise2/DeleFile.java
@@ -1,27 +1,25 @@
 package Exercises.Topice18.Exercise2;
 
 import java.io.File;
-import java.nio.file.Path;
+import java.util.Scanner;
 
 /**
  * 习题2:
  *      删除文件:通过命令行接收文件路径,在程序中将这个文件夹中的所有文件删除,
- *    // todo 通过命令行，你这里是写死的，不符合题目要求
  */
 public class DeleFile {
     public static void main(String[] args) {
-        String forPath = "D:\\DataFile";
+        Scanner scanner = new Scanner(System.in);
+        String forPath = scanner.nextLine();
         File file = new File(forPath);
         String[] tempList = file.list();
-        for (int i = 0 ; i < tempList.length; i++){
-            File temp = new File(forPath+File.separator +tempList[i]);
+        for (String aTempList : tempList) {
+            File temp = new File(forPath + File.separator + aTempList);
             boolean deleFile = temp.delete();
             if (deleFile) {
                 System.out.println("删除成功");
-            }else {
+            } else {
                 System.out.println("删除失败");
-                // todo 多留意ide的提示，这里这个continue有啥用呢
-
             }
         }
     }
