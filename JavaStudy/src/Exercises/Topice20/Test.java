bc5f254013c397e3c51b0858b1244d0b891c6475
Yuan <2365603880@qq.com>
2020-10-19 02:44:11 +0800

    Update#更新20章习题2

--- JavaStudy/src/Exercises/Topice20/Test.java
+++ JavaStudy/src/Exercises/Topice20/Test.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+// FIXME: 2020/10/19  20章习题2
 // FIXME: 2020/10/17  JTextField输入的内容无法显示在JLabel里面   添加了事件  点击确认无法显示   目前就这里出问题   现在思路被磨灭了 今天心态没了明天继续
 
 public class Test {
@@ -22,26 +22,20 @@ public class Test {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(2,1));
        final Container container = frame.getContentPane();
-
         container.add(label = new JLabel("请输入ip地址:"));
-        container.add(text = new JTextField("text") );
-        container.add(button =new JButton("确认"));
-        container.add(button =new JButton("发送"));
+        container.add(text = new JTextField() );
         container.add(button = new JButton("选择文件"));
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String s = button.getText();
-                if (s.equals("发送") && e.getSource() == button) {
-
-                }else if (s.equals("选择文件") && e.getSource() == button){
+                if (s.equals("选择文件") && e.getSource() == button){
+                    String getText = text.getText();
+                    System.out.println("ip地址是:"+getText);
                     JFileChooser fileChooser = new JFileChooser();
                     fileChooser.showOpenDialog(null);
                     System.out.println("路径是:"+fileChooser.getSelectedFile());
-                }else if (s.equals("确认") && e.getSource() == button){
-                   JLabel label2 =  new JLabel();
-                   label2.setText(text.getText());
-                   container.add(label2);
                 }
             }
         });
