d917102c86e0aa069c6757015a929ee140f8a4e6
Yuan <2365603880@qq.com>
2020-10-22 23:01:10 +0800

    ADD#第二阶段代码

---
+++ JXCManager/src/com/lzw/dao/Dao.java
@@ -0,0 +1,32 @@
+package com.lzw.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Dao {
+    protected static String dbClassName = "com.microsoft.jdbc.sqlserver.SQLServerDrive";
+    protected static String dbUrl = "jdbc.microsoft:";
+    protected static String dbUser = "";
+    protected static String dbPwd = "";
+    protected static String second = null;
+    public static Connection conn = null;
+    static{
+        try {
+            if (conn == null) {
+                Class.forName(dbClassName).newInstance();
+                conn = DriverManager.getConnection(dbUrl,dbUser,dbPwd);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean addGys(TbGysinfo gysinfo){
+        if (gysinfo == null) {
+            return false;
+        }
+        return insert("insert tb_gysinfo values(\""+gysinfo.getId()+"\",\""+gysinfo.getName()+"\",\""+gysinfo.getJc()+","+
+                gysinfo.getAddress()+","+gysinfo.getBianma()+","+gysinfo.getTel()+","+gysinfo.getFax()+","+gysinfo.getLian()+","+
+                gysinfo.getLtel()+","+gysinfo.getMail()+","+gysinfo.getYh()+")");
+    }
+}
---
+++ JXCManager/src/com/lzw/dao/TbGysinfo.java
@@ -0,0 +1,12 @@
+package com.lzw.dao;
+
+public class TbGysinfo {
+    private int id;
+    private String name;
+    private String jc;
+    private String getAddress;
+    private int dianHua;
+    private String email;
+    private String bianma;
+
+}
