8a68218844b466cc1d2c14a1de52a2ee3c7597c8
Yuan <2365603880@qq.com>
2020-10-24 16:04:05 +0800

    ADD#@beforeand@After注解

--- Day01/src/cn/itcast/test/CalcuatorTest.java
+++ Day01/src/cn/itcast/test/CalcuatorTest.java
@@ -1,11 +1,28 @@
 package cn.itcast.test;
 
 import cn.itcast.junit.Calculator;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcuatorTest {
 
+    /**
+     * 初始化方法
+     */
+    @Before
+    public void init(){
+        System.out.println("init.....");
+    }
+
+    /**
+     * 释放资源,最后执行
+     */
+    @After
+    public void beat(){
+        System.out.println("beat.....");
+    }
     /**
      * 测试Calcuator类
      */
@@ -14,6 +31,7 @@ public class CalcuatorTest {
         //System.out.println("我被执行了");
         Calculator c = new Calculator();
         int add = c.add(1, 2);
+        System.out.println("testAdd..");
         //System.out.println(add);
         Assert.assertEquals(3,add );
 
