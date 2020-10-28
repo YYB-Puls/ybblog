24ac65237153cf055f64a3bd08a3e8d072741bbe
Yuan <2365603880@qq.com>
2020-10-28 21:37:40 +0800

    ADD#JDBC连接

--- Web/src/main/java/servlet/RegisterServlet.java
+++ Web/src/main/java/servlet/RegisterServlet.java
@@ -18,12 +18,6 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         System.out.println(name);
         System.out.println(password);
-        /*int insert = insert(name, password);
-        if (insert != 0) {
-            response.sendRedirect("/register2.jsp");
-        }else {
-            response.sendRedirect("/register.jsp");
-        }*/
     }
 
     protected void doGet(HttpServletRequest request ,HttpServletResponse response) throws  IOException{
@@ -31,20 +25,6 @@ public class RegisterServlet extends HttpServlet {
     }
 
 
-    public static int insert(String name ,String password){
-        int saveResult = 0;
-        try {
-            Connection connection = JdbcUtil.getConn();
-            String sql = "insert into user(name , password) values(? , ?)";
-            PreparedStatement psmt = connection.prepareStatement(sql);
-            psmt.setString(1, name);
-            psmt.setString(2, password);
-            saveResult = psmt.executeUpdate();
-            JdbcUtil.close(connection, psmt, null);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return saveResult;
-    }
+
 
 }
